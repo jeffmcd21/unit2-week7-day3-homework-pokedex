c0e6cf8b923b35f3ce5e2a8d7a00d25a3998a46f
Jeff McDonald <jwmcdonald85@gmail.com>
2023-12-28 23:44:22 -0500

    Can create a new Pokemon

--- server.js
+++ server.js
@@ -5,11 +5,34 @@ const app = express()
 
 // MIDDLEWARE
 app.use(express.static("public"))
+app.use(express.urlencoded({extended: true}))
+
 
 // INDEX ROUTE
 app.get("/pokemons", (req, res) => {
-    //res.send(pokemons)
+    // res.send(pokemons)
     res.render("index.ejs", {pokemons})
+})
+
+
+// NEW ROUTE
+app.get("/pokemons/new", (req, res) => {
+    res.render("new.ejs")
+})
+
+
+// CREATE ROUTE
+app.post("/pokemons", (req, res) => {
+    const body = req.body
+    // res.send(body)
+    // if (body.stats === "on"){
+    //     body.stats = true
+    // } else {
+    //     body.stats = false
+    // }
+    pokemons.push(body)
+    res.redirect("/pokemons")
+
 })
 
 
@@ -17,7 +40,7 @@ app.get("/pokemons", (req, res) => {
 app.get("/pokemons/:id", (req, res) => {
     const id = req.params.id 
     const pokemon = pokemons[id]
-    //res.send(pokemon)
+    // res.send(pokemon)
     res.render("show.ejs", {pokemon})
 })
 
