7294908aa01be4b2856ae207806f153534779c6f
Jeff McDonald <jwmcdonald85@gmail.com>
2023-12-29 16:31:48 -0500

    Can delete Pokemon

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 
 const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
 const pokemons = require("./models/pokemons.js")
 const app = express()
+
 
 // MIDDLEWARE
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
+app.use(morgan("dev"))
+app.use(methodOverride("_method"))
 
 
 // INDEX ROUTE
@@ -33,6 +38,14 @@ app.post("/pokemons", (req, res) => {
     pokemons.push(body)
     res.redirect("/pokemons")
 
+})
+
+
+// DELETE ROUTE
+app.delete("/pokemons/:id", (req, res) => {
+    const id = req.params.id 
+    pokemons.splice(id, 1)
+    res.redirect("/pokemons")
 })
 
 
