dfbb3b82ba91724201b98160880b045d43a4f94c
Jeff McDonald <jwmcdonald85@gmail.com>
2024-01-03 09:44:03 -0500

    elaborated on functionality by updating edit, new & create

--- controllers/pokemons.js
+++ controllers/pokemons.js
@@ -21,18 +21,37 @@ router.get("/new", (req, res) => {
 
 // CREATE ROUTE
 router.post("/", (req, res) => {
-    const body = req.body
+    let newPokesInfo = {
+        name: req.body.name,
+        img: req.body.img,
+        type: [req.body.type[0], req.body.type[1]],
+        stats: {
+          hp: req.body.hp,
+          attack: req.body.attack,
+          defense: req.body.defense,
+          spattack: req.body.spattack,
+          spdefense: req.body.spdefense,
+          speed: req.body.spdefense,
+        },
+      }
+      req.body = newPokesInfo;
+    // const body = req.body
     // res.send(body)
     // if (body.stats === "on"){
     //     body.stats = true
     // } else {
     //     body.stats = false
     // }
-    body.stats = {
-        speed: req.body.speed
-    }
-    console.log(body)
-    pokemons.push(body)
+    // body.stats = {
+    //     hp: req.body.hp,
+    //     attack: req.body.attack,
+    //     defense: req.body.defense,
+    //     spattack: req.body.spattack,
+    //     spdefense: req.body.spdefense,
+    //     speed: req.body.speed,
+    // }
+    // console.log(body)
+    pokemons.push(req.body)
     res.redirect("/pokemons")
 
 })
@@ -49,22 +68,52 @@ router.delete("/:id", (req, res) => {
 router.get("/:id/edit", (req, res) => {
     const id = req.params.id 
     const pokemon = pokemons[id]
-    res.render("pokemons/edit.ejs", {pokemon, id})
+    res.render("pokemons/edit.ejs", {pokemons, pokemon, id})
 })
 
 
 // UPDATE ROUTE
 router.put("/:id", (req, res) => {
-    const id = req.params.id 
-    const body = req.body 
+    let updatePokes = {
+        name: req.body.name,
+        type: req.body.type.split(" "),
+        stats: {
+          hp: req.body.hp,
+          attack: req.body.attack,
+          defense: req.body.defense,
+          spattack: req.body.spattack,
+          spdefense: req.body.spdefense,
+          speed: req.body.spdefense,
+        },
+      };
+      let currentPokes = pokemons[req.params.id];
+      req.body = updatePokes;
+      let overWrite = {
+        ...currentPokes,
+        ...req.body,
+      }
+      pokemons[req.params.id] = overWrite;
+      res.redirect("/pokemons");
+    });
+    
+    // const id = req.params.id 
+    // const body = req.body 
+    // body.stats = {
+    //     hp: req.body.hp,
+    //     attack: req.body.attack,
+    //     defense: req.body.defense,
+    //     spattack: req.body.spattack,
+    //     spdefense: req.body.spdefense,
+    //     speed
+    // }
     // if (body.stats === "on"){
     //     body.stats = true
     // } else {
     //     body.stats = false
     // }
-    pokemons[id] = body 
-    res.redirect("/pokemons")
-})
+//     pokemons[id] = body 
+//     res.redirect("/pokemons")
+// })
 
 
 // SHOW ROUTE
@@ -72,7 +121,7 @@ router.get("/:id", (req, res) => {
     const id = req.params.id 
     const pokemon = pokemons[id]
     // res.send(pokemon)
-    res.render("pokemons/show.ejs", {pokemon, id})
+    res.render("pokemons/show.ejs", {pokemons, pokemon, id})
 })
 
 
